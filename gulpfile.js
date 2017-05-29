93971f7a5c5aebc52b01fe8cc6b81dc6b768b78e
Chetan Gawade <chetan@newput.com>
2017-05-29 14:51:13 +0530

    Make changes into package.json n gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -83,11 +83,20 @@ gulp.task('set-env', function () {
     env({
       vars: {
         //MONGO_URI: "mongodb://chetan:download1@ds133279.mlab.com:33279/vod",
-        MONGO_URI: "mongodb://chetan:chet@n2109@ds155811.mlab.com:55811/product-newput",
+        MONGO_URI: "mongodb://chetan:@ds155811.mlab.com:55811/product-newput",
         PORT: 9001
       }
     });
   }
 });
+
+gulp.task('heroku-build',['inject-files'], function(){
+  env({
+      vars: {
+        MONGO_URI: "mongodb://chetan:@ds155811.mlab.com:55811/product-newput",
+        PORT: 3000
+      }
+    });
+})
 
 gulp.task('default', ['set-env','server', 'watch']);

8c01e3c2df06d462b7ddbf211ced72de684440f5
Chetan Gawade <chetan@newput.com>
2017-06-02 15:31:15 +0530

    Correct req.param param in rest/app

--- routes/rest.js
+++ routes/rest.js
@@ -4,10 +4,10 @@ var router = express.Router();
 router.get('/app', function (req, res, next) {
   var challange;
   console.log("in rest/app req.params :: ",
-  req.param('hub.verify_token'), "req.body ::", req.body, "req.params :: ",req.params );
-  var verify_token = req.params['hub.verify_token'];
-  if (verify_token === '12345678' && params['hub.challenge']) {
-    challange = parseInt(req.params['hub.challenge']);
+  req.param('hub.verify_token'), req.param('hub.challenge'));
+  var verify_token = req.param('hub.verify_token');
+  if (verify_token === '12345678' && req.param('hub.challenge')) {
+    challange = parseInt(req.param('hub.challenge'));
   }
   res.send({
     statusCode: 200,

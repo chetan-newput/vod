ac834331b14bd1c4fb32dbb4175fed364f0d6d36
Chetan Gawade <chetan@newput.com>
2017-06-02 16:56:38 +0530

    set up post rest/app

--- routes/rest.js
+++ routes/rest.js
@@ -13,8 +13,8 @@ router.get('/app', function (req, res, next) {
 });
 
 router.post('/app', function (req, res, next) {
-  console.log("in rest/app req.params :: ");
-  console.log(req);
+  console.log("in post rest/app req.params :: ",
+  req.params, "res.body :: ",req.body, "req.query :: ",req.query);
   // EAAGGrMEDRSkBAM19T8OWTyi1NK8FpoVuVZCKbxZCNjiIlbTOaonmiw8x1cBEF3O5shnskhKza3Buyfgr7eoK0rf7fn2ZBZBli3nQYOwJJJlZALkDPWcPZAKTP6E22wEBFh8ZBZA8vZBwgB1hi6Jal7RqPB9p5EUyDMNFfYP4my9zWEgZDZD
 
   // req.param('hub.verify_token'), req.param('hub.challenge'));

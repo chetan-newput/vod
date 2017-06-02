6cee8aa8f82f299dad8da1e28cbec874afe1491f
Chetan Gawade <chetan@newput.com>
2017-06-02 16:29:54 +0530

    Correct the webhook api

--- routes/rest.js
+++ routes/rest.js
@@ -2,22 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/app', function (req, res, next) {
-  var challange;
   console.log("in rest/app req.params :: ",
   req.param('hub.verify_token'), req.param('hub.challenge'));
   var verify_token = req.param('hub.verify_token');
   if (verify_token === '12345678' && req.param('hub.challenge')) {
-    challange = parseInt(req.param('hub.challenge'));
+    res.send(req.param('hub.challenge'));
+  } else {
+    res.sendStatus(400);
   }
-  var newRes = {
-    status: 200,
-    body: challange
-  };
-  console.log('newRes');
-  console.log(newRes);
-  //res.json(newRes);
-  // res.status(200).send(challange);
-  res.send(req.param('hub.challenge'));
+  
+  
 });
 
 module.exports = router;

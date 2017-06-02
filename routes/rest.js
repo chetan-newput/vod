6a42cf1e70becb775c738803d6d85989f5b80d51
Chetan Gawade <chetan@newput.com>
2017-06-02 15:25:44 +0530

    =check params on rest/app

--- routes/rest.js
+++ routes/rest.js
@@ -3,12 +3,13 @@ var router = express.Router();
 
 router.get('/app', function (req, res, next) {
   var challange;
-  console.log("in rest/app req.params :: ",req.params['hub.verify_token']);
+  console.log("in rest/app req.params :: ",
+  req.param('hub.verify_token'), "req.body ::", req.body, "req.params :: ",req.params );
   var verify_token = req.params['hub.verify_token'];
   if (verify_token === '12345678' && params['hub.challenge']) {
     challange = parseInt(req.params['hub.challenge']);
   }
-  res.json({
+  res.send({
     statusCode: 200,
     body: challange
   });

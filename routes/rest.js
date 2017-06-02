653b7a2e85f0c8ba3b2c92cad0380ae75dc75931
Chetan Gawade <chetan@newput.com>
2017-06-02 16:39:57 +0530

    Correct the webhook api-1

--- routes/rest.js
+++ routes/rest.js
@@ -10,7 +10,18 @@ router.get('/app', function (req, res, next) {
   } else {
     res.sendStatus(400);
   }
-  
+});
+
+router.post('/app', function (req, res, next) {
+  console.log("in rest/app req.params :: ");
+  console.log(req);
+  // req.param('hub.verify_token'), req.param('hub.challenge'));
+  // var verify_token = req.param('hub.verify_token');
+  // if (verify_token === '12345678' && req.param('hub.challenge')) {
+  //   res.send(req.param('hub.challenge'));
+  // } else {
+  //   res.sendStatus(400);
+  // }
   
 });
 
